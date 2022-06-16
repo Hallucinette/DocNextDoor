6463166e764c1220e7a5e8c4d1631ec0d5878f11
Amelie Pocchiolo <ameliepocchiolo@gmail.com>
2022-06-16 16:46:19 +0200

    Creation fichier + .ignore

---
+++ DocNextDoor/DocNextDoorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocNextDoorApp.swift
+//  DocNextDoor
+//
+//  Created by apprenant59 on 16/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct DocNextDoorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
