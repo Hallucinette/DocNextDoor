7a0f418950f438b636181e634a838c4134818898
Amelie Pocchiolo <ameliepocchiolo@gmail.com>
2022-06-17 16:21:34 +0200

    creation branch

---
+++ BubbleTabbar/BubbleTabbarView.swift
@@ -0,0 +1,20 @@
+//
+//  BubbleTabbarView.swift
+//  DocNextDoor
+//
+//  Created by apprenant59 on 17/06/2022.
+//
+
+import SwiftUI
+
+struct BubbleTabbarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BubbleTabbarView_Previews: PreviewProvider {
+    static var previews: some View {
+        BubbleTabbarView()
+    }
+}
