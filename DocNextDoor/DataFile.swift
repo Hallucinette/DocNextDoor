ace040dd0cf1089db30170141d1af7dafbb9152f
Amelie Pocchiolo <ameliepocchiolo@gmail.com>
2022-06-17 16:54:43 +0200

    gdnjdsd

--- DocNextDoor/DataFile.swift
+++ DocNextDoor/DataFile.swift
@@ -15,6 +15,7 @@ struct ContactData {
     ]
 }
 
+<<<<<<< Updated upstream
 // A struct to store exactly one Advertisement's data.
 struct AdvertisementData {
     // Create Advertisement, then show them in a list.
@@ -23,3 +24,13 @@ struct AdvertisementData {
     ]
     
 }
+=======
+//// A struct to store exactly one Advertisement's data.
+//struct AdvertisementData {
+//    // Create Advertisement, then show them in a list.
+//    let ads: [Advertisement] = [
+//        Advertisement( title: "Médecin généraliste à Plumebec (51420)", town: "Plumebec", zipCode: 51420, description: "En Morbihan Sud, la commune de Plumelec se situant à 20 min de Vannes,1 heure de Rennes et 1heure30 de Nantes recherche un médecin généraliste. Cette commune de 2750 habitants concilie tranquillité de la campagne, vitalité économique et touristique . En effet Plumelec regroupe tous les services nécessaire pour bien y vivre : nombreux artisans et commerces; nombreuses associations; nombreux équipements sportifs et culturels; un nouveau centre de secours comptant 33 pompiers volontaires ainsi qu'un pôle médical regroupant plusieurs professionnels de la santé : 1 médecin, un pédicure podologue, 4 infirmières, 3 kinés et un orthophoniste. Hors pôle médical la commune compte également un dentiste, un ostéopathe, un service associatif de soins et de maintien à domicile, un service", contact: "Mairie de PLUMELEC", pict: [""], speciality: "", patientsList: true, accomodationProvided: true)
+//    ]
+//    
+//}
+>>>>>>> Stashed changes
