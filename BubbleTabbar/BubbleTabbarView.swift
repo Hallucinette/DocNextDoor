c0fbc966627dab798eb1ea88623d8ff6de056a10
Amelie Pocchiolo <ameliepocchiolo@gmail.com>
2022-06-17 16:49:25 +0200

    debut tabbar

--- BubbleTabbar/BubbleTabbarView.swift
+++ BubbleTabbar/BubbleTabbarView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct BubbleTabbarView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Home()
+        
     }
 }
 
@@ -18,3 +19,50 @@ struct BubbleTabbarView_Previews: PreviewProvider {
         BubbleTabbarView()
     }
 }
+
+struct Home: View{
+    
+    @State var selectedtab = "liste"
+    
+    init(){
+        UITabBar.appearance().isHidden = true
+    }
+    var body: some View{
+        ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
+            TabView(selection: $selectedtab){
+                    
+                    Color.red
+                        .ignoresSafeArea(.all, edges: .all)
+                        .tag("liste")
+                    
+                    Color.blue
+                        .ignoresSafeArea(.all, edges: .all)
+                        .tag("map")
+                    Color.pink
+                        .ignoresSafeArea(.all, edges: .all)
+                        .tag("bookmark")
+                    Color.purple
+                        .ignoresSafeArea(.all, edges: .all)
+                        .tag("profil")
+            }
+            
+            // Custom tab Bar...
+            
+            HStack(spacing: 0){
+                
+                ForEach(tabs, id: \.self){image in
+                    Button(action: {}, label: {
+                        Image(systemName: image)
+                            .resizable()
+                            .renderingMode(.template)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 25, height: 25)
+                    })
+                    if image != tabs.last{Spacer(minLength: 0)}
+                }
+            }
+        }
+    }
+}
+
+    var tabs = ["list.bullet.circle", "map", "bookmark", "person.circle"]
