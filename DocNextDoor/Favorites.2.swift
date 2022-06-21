1024d2b72eaea20d26ce81dc78f5f9fd29112674
Asysha <contact.marie.css@gmail.com>
2022-06-21 08:08:35 -0700

    view favorites FINIE / ajout des fonctions pour les swipeActions

--- DocNextDoor/Favorites.2.swift
+++ DocNextDoor/Favorites.2.swift
@@ -7,19 +7,41 @@
 import SwiftUI
 import Foundation
 
-
-func supprAd(@State list: [Advertisement]) {
-    //a revoir cf internet
-}
 func transferAd(@State atransf: Advertisement, @State list: [Advertisement] ) {
     //list.append(atransf)
     //supprAd(atransf)
 }
 
 struct Favorites_2: View {
-    @State var profilUser : ProfilUser
+    @State var profilUser : ProfilUser //a passer en binding ?
     @State var pickerList = 0
     
+    func deleteAdF(ad: Advertisement) {
+        profilUser.adsFav.removeAll { ad2 in
+            return ad.id == ad2.id
+        }
+    }
+    
+    func deleteAdA(ad: Advertisement) {
+        profilUser.adsApplied.removeAll { ad2 in
+            return ad.id == ad2.id
+        }
+    }
+    
+    func transfAdF2ADA(ad: Advertisement) {
+        profilUser.adsApplied.append(ad)
+        profilUser.adsFav.removeAll { ad2 in
+            return ad.id == ad2.id
+        }
+    }
+    
+    func transfAdA2AdF(ad: Advertisement) {
+        profilUser.adsFav.append(ad)
+        profilUser.adsApplied.removeAll { ad2 in
+            return ad.id == ad2.id
+        }
+    }
+
     var body: some View {
         ZStack { //background
             Color("BackG").ignoresSafeArea()
@@ -54,7 +76,6 @@ struct Favorites_2: View {
                         ForEach( pickerList == 1 ? profilUser.adsApplied : profilUser.adsFav) {  ad in //affichage en liste
                             HStack {
                                 NavigationLink(destination: DetailledAd()){ //add parametre (ad: ad) --> DetailledAd((ad: ad))
-                                
                                     Image(ad.pict[0]).resizable().overlay(Circle().stroke(Color("Darkblue"), lineWidth: 4)).clipShape(Circle())//stroke Color = "\(ad.speciality)Color"
                                         .frame(width: 55, height: 55).padding(3)
                                     VStack {
@@ -75,35 +96,40 @@ struct Favorites_2: View {
                                             Text(ad.description).foregroundColor(Color("Txtgrey")).font(.footnote).padding(3)
                                         }.frame(height: 50)
                                     }//fin Vsatck txt liste = titre, specialités, ville + CP et debut description
-                            }.navigationTitle("")//fin navigationLink
-                            }//fin HSatck 1 element de la liste
-                        }.listRowBackground(Color("BackG")).swipeActions(allowsFullSwipe: false) { //action on swipe
-                            Button() {
-                                print("Annonce retirée des favoris")
-                            } label: {
-                                Label("Supprimer", systemImage: "trash.fill")
-                            }.tint(.red)
-                            if pickerList == 0 {
-                                Button () {
-                                    print("Annonce deplacée dans Annonces candidatées")
+                                }.navigationTitle("")//fin navigationLink
+                            }.swipeActions(allowsFullSwipe: false) { //action on swipe and fin for each
+                                Button {
+                                    if pickerList == 1 {
+                                        deleteAdA(ad: ad)
+                                    }
+                                    else {
+                                        deleteAdF(ad: ad)
+                                    }
                                 } label: {
-                                    Label("Candidatée", systemImage: "paperplane.fill")
-                                }
-                                .tint(Color("Darkblue"))//fin Hstack 1 element de la list
-                            }//fin if button
-                                else {
-                                    Button () {
-                                        print("Annonce deplacée dans Annonces favorites")
+                                    Label("Supprimer", systemImage: "trash.fill")
+                                }.tint(.red)
+                                if pickerList == 0 {
+                                    Button {
+                                        transfAdF2ADA(ad: ad)
                                     } label: {
-                                        Label("Favoris", systemImage: "star.fill")
+                                        Label("Déplacer dans Ann. candidatées", systemImage: "paperplane.fill")
                                     }
                                     .tint(Color("Lightblue"))//fin Hstack 1 element de la list
+                                }//fin if button
+                                else {
+                                        Button {
+                                            transfAdA2AdF(ad: ad)
+                                            print("Annonce deplacée dans Annonces favorites")
+                                        } label: {
+                                            Label("Déplacer dans Ann. favorites", systemImage: "star.fill")
+                                        }
+                                        .tint(Color("Lightblue"))
                                 }//fin else button
-                            //fin Hstack 1 element de la list
-                        }//fin For Each
+                            }//fin action on swipe//fin HSatck 1 element de la liste
+                        }.listRowBackground(Color("BackG"))
                     }.frame(width: 440, alignment: .center)//fin list
                 }//fin else list non vide
-                }//fin Vstack full view
+            }//fin Vstack full view
         }//fin ZStack background color
     }//end body
 }//end Favorites View
@@ -114,4 +140,5 @@ struct FFavorites_2_Previews: PreviewProvider {
         Favorites_2(profilUser: .init(isDoc: true, name: "Annie lhation", pp: "P1", speciality: "Médecin généraliste", description: "Ecole de médecine de Marseille", contact: .init(name:  "Annie lhation", mail: "monmail@gg.com"), allowNotif: true, adsApplied: [Advertisement.init( title: "Médecin à Plumebec", town: "Plumebec", zipCode: "51420", description: "En Morbihan Sud, la commune de Plumelec se situant à 20 min de Vannes,1 heure de Rennes et 1heure30 de Nantes recherche un médecin généraliste. Cette commune de 2750 habitants concilie tranquillité de la campagne, vitalité économique et touristique . En effet Plumelec regroupe tous les services nécessaire pour bien y vivre : nombreux artisans et commerces; nombreuses associations; nombreux équipements sportifs et culturels; un nouveau centre de secours comptant 33 pompiers volontaires ainsi qu'un pôle médical regroupant plusieurs professionnels de la santé : 1 médecin, un pédicure podologue, 4 infirmières, 3 kinés et un orthophoniste. Hors pôle médical la commune compte également un dentiste, un ostéopathe, un service associatif de soins et de maintien à domicile, un service", contact: .init(name: "Mairie de PLUMELEC", mail: ""), pict: [""], speciality: "Médecin généraliste", patientsList: true, accomodationProvided: true), Advertisement.init( title: "Médecin à Tours", town: "Tours", zipCode: "XXXXX", description: "En Morbihan Sud, la commune de Plumelec se situant à 20 min de Vannes,1 heure de Rennes et 1heure30 de Nantes recherche un médecin généraliste. Cette commune de 2750 habitants concilie tranquillité de la campagne, vitalité économique et touristique . En effet Plumelec regroupe tous les services nécessaire pour bien y vivre : nombreux artisans et commerces; nombreuses associations; nombreux équipements sportifs et culturels; un nouveau centre de secours comptant 33 pompiers volontaires ainsi qu'un pôle médical regroupant plusieurs professionnels de la santé : 1 médecin, un pédicure podologue, 4 infirmières, 3 kinés et un orthophoniste. Hors pôle médical la commune compte également un dentiste, un ostéopathe, un service associatif de soins et de maintien à domicile, un service", contact: .init(name: "Mairie de PLUMELEC", mail: ""), pict: [""], speciality: "Médecin généraliste", patientsList: true, accomodationProvided: true)], adsFav: [])).previewDevice(PreviewDevice(rawValue: "iPhone 13"))
     }
 }
+
 
