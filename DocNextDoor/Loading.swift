fd44658eec2af63d21074b3d081bdf8db306c244
Asysha <contact.marie.css@gmail.com>
2022-06-22 07:42:25 -0700

    sauvegarde

--- DocNextDoor/Loading.swift
+++ DocNextDoor/Loading.swift
@@ -17,7 +17,7 @@ struct Loading: View {
                 Image("Loading").resizable().aspectRatio(contentMode: .fit).frame(width: 350, height: 280, alignment: .center).padding(10)
                 Rectangle().frame(width: 80, height: 3, alignment: .center).foregroundColor(Color("Lightblue")).padding(15)
                 Text("DocNextDoor").font(Font.custom("FredokaOne-Regular", size: 42)).bold().multilineTextAlignment(.center).foregroundColor(Color(.white)).frame(width: 350).padding(5)
-                Text("Slogan  !").font(.title3).italic().multilineTextAlignment(.center).foregroundColor(Color("Txtgrey")).frame(width: 350)
+                Text("Des opportunités pour lutter contre les déserts médicaux !").font(.title3).italic().multilineTextAlignment(.center).foregroundColor(Color(.white)).frame(width: 300)
                 Rectangle().frame(width: 80, height: 3, alignment: .center).foregroundColor(Color("Lightblue")).padding(20)
                 Spacer()
             }
