a9de11827ababb6fa5e7e987cdb92a5b39bbeb50
Asysha <contact.marie.css@gmail.com>
2022-06-22 02:31:49 -0700

    Ajout des PP dans assets et de la view Profile

---
+++ DocNextDoor/Settings.swift
@@ -0,0 +1,20 @@
+//
+//  Settings.swift
+//  DocNextDoor
+//
+//  Created by Apprenant 11 on 22/06/2022.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        Settings()
+    }
+}
