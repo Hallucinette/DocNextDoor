6547a63a30ac13682c0aae6238cff1c7854318b9
pdgswift <paridenis1@gmail.com>
2022-06-23 12:15:58 +0200

    merge code error resolution

--- DocNextDoor/Structures.swift
+++ DocNextDoor/Structures.swift
@@ -84,13 +84,8 @@ struct ProfilUser : Identifiable {
 
     var allowNotif : Bool
         
-<<<<<<< HEAD
     var adsApplied : [Advertisement]
         
     var adsFav : [Advertisement]
-=======
-    var adsApplied : [Advertisement]?
-        
-    var adsFav : [Advertisement]?
->>>>>>> pdg
+
 }
