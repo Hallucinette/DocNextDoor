a0ad42179094c4e5bdfb34d8c0c92fb2197be82e
digiDreams <digital2dreamer@gmail.com>
2022-06-24 19:24:24 +0200

    Fix instances call from SlidersViewRow --> FiltersViewRow in FiltersView
    "FiltersView Rebuild"

--- DocNextDoor/Search/Filters/FiltersView.swift
+++ DocNextDoor/Search/Filters/FiltersView.swift
@@ -38,7 +38,6 @@ struct FiltersView: View {
                         Text("Logement mis à disposition")  // toggleswitch
                     }
                     Group {
-
                         FiltersViewRow(sliderValue: $maxRent, sliderLabel: "Montant maximum du loyer", sliderUnit: "EUR", sliderRangeMin: 0.0, sliderRangeMax: 10_000.0, sliderStep: 50.0).tag("RentSlider")
                             .tag("rentValue")
                         FiltersViewRow(sliderValue: $maxDistSchool, sliderLabel: "Distance maximum école / crèche", sliderUnit: "Km", sliderRangeMin: 0.0, sliderRangeMax: 100.0, sliderStep: 5.0).tag("DistSchoolSlider")
@@ -47,7 +46,6 @@ struct FiltersView: View {
                             .tag("IDZValue")
                         FiltersViewRow(sliderValue: $maxDistHospital, sliderLabel: "Distance maximum hôpital / maison médicale de garde", sliderUnit: "Km", sliderRangeMin: 0.0, sliderRangeMax: 100.0, sliderStep: 5.0).tag("DistHospitalSlider")
                             .tag("hospitalValue")
-                     
                     }
                     Group {
                         Text("Transports à proximité") // Logos
