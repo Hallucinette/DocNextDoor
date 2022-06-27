96ce5e6b752e2f6aaf584e5ad4f012ba5b67f7fe
digiDreams <digital2dreamer@gmail.com>
2022-06-27 16:59:50 +0200

    Commit DataFile + FiltersView

--- DocNextDoor/View/Filters/FiltersView.swift
+++ DocNextDoor/View/Filters/FiltersView.swift
@@ -39,9 +39,15 @@ struct FiltersView: View {
                     }
                     Group {
                         FiltersViewRow(sliderValue: maxRent, sliderLabel: "Montant maximum du loyer", sliderUnit: "EUR", sliderRangeMin: 0.0, sliderRangeMax: 10_000.0, sliderStep: 50.0).tag("RentSlider")
+                            .tag("rentValue")
                         FiltersViewRow(sliderValue: maxDistSchool, sliderLabel: "Distance maximum école / crèche", sliderUnit: "Km", sliderRangeMin: 0.0, sliderRangeMax: 100.0, sliderStep: 5.0).tag("DistSchoolSlider")
+                            .tag("schoolValue")
+                        
+                        
                         FiltersViewRow(sliderValue: maxDistIDZ, sliderLabel: "Distance maximum centre commercial / ZAC", sliderUnit: "Km", sliderRangeMin: 0.0, sliderRangeMax: 100.0, sliderStep: 5.0).tag("DistIDZSlider")
+                            .tag("IDZValue")
                         FiltersViewRow(sliderValue: maxDistHospital, sliderLabel: "Distance maximum hôpital / maison médicale de garde", sliderUnit: "Km", sliderRangeMin: 0.0, sliderRangeMax: 100.0, sliderStep: 5.0).tag("DistHospitalSlider")
+                            .tag("hospitalValue")
                         
                     }
                     Group {
@@ -51,7 +57,7 @@ struct FiltersView: View {
                 }
                 .padding()
                 .navigationTitle("Filtres")
-                Button {
+                Button{ //(alignment: .center)
                     // apply and save userFilters
                     //                .onTapGesture {
                     //                       print("Filtres appliqués")
@@ -61,11 +67,12 @@ struct FiltersView: View {
                         .background(.blue)
                         .foregroundColor(.white)
                         .clipShape(Capsule())
-                }
+                } // .alignment(center)
                 
                 //            Text("sliderValue.maxRent:\(sliderValue.maxRent)") //aTest
                 //            var aTest = RentSlider.maxRent
                 //            Text("aTest:\(aTest)")
+                Text("La valeur de maxRent est de: \(maxRent)") //rentValue.maxRent, $maxRent.sliderValue
             }
         }
     }
