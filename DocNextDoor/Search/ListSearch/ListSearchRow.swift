b7b1eec0c039e3917351af6225bc64c65a781c1c
digiDreams <digital2dreamer@gmail.com>
2022-06-30 00:05:49 +0200

    Work on List ads display

--- DocNextDoor/Search/ListSearch/ListSearchRow.swift
+++ DocNextDoor/Search/ListSearch/ListSearchRow.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct ListSearchRow: View {
-  
+    
     @Binding var ad: Advertisement
-    @Binding var star : ColoredIcons
+    @Binding var starIcon : ColoredIcons
     @Binding var user : ProfilUser
+    @State var favIcon: String = "star"
     
     var body: some View {
-    
+//        NavigationLink {
+//            // Destination View(ad: ad)
+//        } label: {
         HStack {
             Image(ad.pict[0])
                 .resizable()
@@ -38,38 +41,51 @@ struct ListSearchRow: View {
                 Text(ad.description)
                     .foregroundColor(.gray)
             }
+//        }
             Button (action: {
-                user.adsFav.append(ad)
+                if favIcon == "star" {
+                    user.adsFav.append(ad)
+                    favIcon = "star.fill"
+                } else {
+                    func removeFav(ad: Advertisement) {
+                        user.adsFav.removeAll { deletedValue in
+                            return ad.id == deletedValue.id
+                        }
+                    }
+                    favIcon = "star"
+                }
+                
             }, label: {
-                Image(systemName: "star")
+                Image(systemName: favIcon)
                     .font(.largeTitle)
                     .padding()
+                    .foregroundColor(Color("Darkblue"))
                 
-//                Image(star.isColored == true ? star.nameIconColored : star.nameIcon)
-//                    .font(.largeTitle)
-//                    .foregroundColor(Color("Darkblue"))
-//                    .frame(maxWidth: 8, maxHeight: 8)
-            
+                //                Image(starIcon.isColored == true ? starIcon.nameIconColored : starIcon.nameIcon)
+                //                    .font(.largeTitle)
+                //                    .foregroundColor(Color("Darkblue"))
+                //                    .frame(maxWidth: 8, maxHeight: 8)
+                
             }) // .clipShape(Circle())
-            
-            
         }
         
         
-         
-         
-         
         
-      
-     
-       
         
+        
+        
+        
+        
+        
+        
+        
+    }
+}
 
-            }
-        }
-        
-        struct ListSearchRow_Previews: PreviewProvider {
-            static var previews: some View {
-                ListSearchRow(ad: .constant (Advertisement(title: "MedGen à Toulon", town: "Toulon", zipCode: "83000", description: "Plage et beau temps, patientèle patiente.", benefits: "Facilités d'installation", contact: Contact(name: "SEA Nirre", mail: "vacHealth@fake.com", phone: "0601020304"), icons: ["ecoleC", "shopC"], transport: [ColoredIcons(nameIcon: "metroG", nameIconColored: "metroC", isColored: true), ColoredIcons(nameIcon: "trainG", nameIconColored: "trainC", isColored: true)], zone: [ColoredIcons(nameIcon: "arsG", nameIconColored: "arsC", isColored: true),ColoredIcons(nameIcon: "zacG", nameIconColored: "zacC", isColored: true)], pict: ["Grande-synthe"], speciality: "Médecine générale", patientsList: true, accomodationProvided: false)), star: .constant (ColoredIcons(nameIcon: "star", nameIconColored: "star.fill", isColored: true)), user: .constant (ProfilUser(id: UUID(), isDoc: true, name: "Isabella Zoe", pp: "Female4", speciality: "medGen", description: "J'ai fait l'école de médecine de Marseille, dont je suis diplomée depuis 2020.", contact: Contact(name: "Isabella Zoe", mail: "monmail@lol.fr", phone: "0600000000"), adsPublished: [Advertisement(title: "Cardiologue à Lille", town: "Lille", zipCode: "59000", description: "Mauvais temps, beaucoup de travail, pas de vie", benefits: "Soutien moral et bières.", contact: Contact(name: "Enjoy LIFE", mail: "sossauvezmoi@help.com", phone: "0070070070"), icons: ["ecoleC", "shopC"], transport: [ColoredIcons(nameIcon: "metroG", nameIconColored: "metroC", isColored: true), ColoredIcons(nameIcon: "trainG", nameIconColored: "trainC", isColored: true)], zone: [ColoredIcons(nameIcon: "arsG", nameIconColored: "arsC", isColored: true),ColoredIcons(nameIcon: "zacG", nameIconColored: "zacC", isColored: true)], pict: ["Plumebec"], speciality: "Psychologue", patientsList: false, accomodationProvided: false)], cv: "CV3", allowContact: true, allowNotif: true, adsApplied: [Advertisement(title: "Farniente à Pau", town: "Pau", zipCode: "64000", description: "Vaches, cochons, et accessoiremment êtres humains à soigner", benefits: "Transat fourni", contact: Contact(name: "Jean FOUPASUNE", mail: "poildanslamain@injoignable.fr", phone: "0700000000"), icons: ["ecoleC", "shopC"], transport: [ColoredIcons(nameIcon: "metroG", nameIconColored: "metroC", isColored: true), ColoredIcons(nameIcon: "trainG", nameIconColored: "trainC", isColored: true)], zone: [ColoredIcons(nameIcon: "arsG", nameIconColored: "arsC", isColored: true),ColoredIcons(nameIcon: "zacG", nameIconColored: "zacC", isColored: true)], pict: ["Plumebec"], speciality: "Podologue", patientsList: false, accomodationProvided: false)], adsFav: [])))
-            }
-        }
+struct ListSearchRow_Previews: PreviewProvider {
+    static var previews: some View {
+//        NavigationView {
+        ListSearchRow(ad: .constant (Advertisement(title: "MedGen à Toulon", town: "Toulon", zipCode: "83000", description: "Plage et beau temps, patientèle patiente.", benefits: "Facilités d'installation", contact: Contact(name: "SEA Nirre", mail: "vacHealth@fake.com", phone: "0601020304"), icons: ["ecoleC", "shopC"], transport: [ColoredIcons(nameIcon: "metroG", nameIconColored: "metroC", isColored: true), ColoredIcons(nameIcon: "trainG", nameIconColored: "trainC", isColored: true)], zone: [ColoredIcons(nameIcon: "arsG", nameIconColored: "arsC", isColored: true),ColoredIcons(nameIcon: "zacG", nameIconColored: "zacC", isColored: true)], pict: ["Grande-synthe"], speciality: "Médecine générale", patientsList: true, accomodationProvided: false)), starIcon: .constant (ColoredIcons(nameIcon: "star", nameIconColored: "star.fill", isColored: true)), user: .constant (ProfilUser(id: UUID(), isDoc: true, name: "Isabella Zoe", pp: "Female4", speciality: "medGen", description: "J'ai fait l'école de médecine de Marseille, dont je suis diplomée depuis 2020.", contact: Contact(name: "Isabella Zoe", mail: "monmail@lol.fr", phone: "0600000000"), adsPublished: [Advertisement(title: "Cardiologue à Lille", town: "Lille", zipCode: "59000", description: "Mauvais temps, beaucoup de travail, pas de vie", benefits: "Soutien moral et bières.", contact: Contact(name: "Enjoy LIFE", mail: "sossauvezmoi@help.com", phone: "0070070070"), icons: ["ecoleC", "shopC"], transport: [ColoredIcons(nameIcon: "metroG", nameIconColored: "metroC", isColored: true), ColoredIcons(nameIcon: "trainG", nameIconColored: "trainC", isColored: true)], zone: [ColoredIcons(nameIcon: "arsG", nameIconColored: "arsC", isColored: true),ColoredIcons(nameIcon: "zacG", nameIconColored: "zacC", isColored: true)], pict: ["Plumebec"], speciality: "Psychologue", patientsList: false, accomodationProvided: false)], cv: "CV3", allowContact: true, allowNotif: true, adsApplied: [Advertisement(title: "Farniente à Pau", town: "Pau", zipCode: "64000", description: "Vaches, cochons, et accessoiremment êtres humains à soigner", benefits: "Transat fourni", contact: Contact(name: "Jean FOUPASUNE", mail: "poildanslamain@injoignable.fr", phone: "0700000000"), icons: ["ecoleC", "shopC"], transport: [ColoredIcons(nameIcon: "metroG", nameIconColored: "metroC", isColored: true), ColoredIcons(nameIcon: "trainG", nameIconColored: "trainC", isColored: true)], zone: [ColoredIcons(nameIcon: "arsG", nameIconColored: "arsC", isColored: true),ColoredIcons(nameIcon: "zacG", nameIconColored: "zacC", isColored: true)], pict: ["Plumebec"], speciality: "Podologue", patientsList: false, accomodationProvided: false)], adsFav: [])))
+//        }
+    }
+}
--- DocNextDoor/Search/ListSearch/ListSearchView.swift
+++ DocNextDoor/Search/ListSearch/ListSearchView.swift
@@ -9,17 +9,23 @@ import SwiftUI
 
 struct ListSearchView: View {
     
-//    @State var profilUser : ProfilUser  = profilUser(isDoc: true, name: "Annie Hilation", pp: "P1", speciality: "Médecin généraliste", description: "Ecole de médecine de Marseille", contact: .init(name:  "Annie lhation", mail: "monmail@gg.com"), allowContact: true, allowNotif: true, adsApplied: [], adsFav: [Advertisement.init( title: "Médecin à Plumebec", town: "Plumebec", zipCode: "51420", description: "En Morbihan Sud, la commune de Plumelec se situant à 20 min de Vannes,1 heure de Rennes et 1heure30 de Nantes recherche un médecin généraliste. Cette commune de 2750 habitants concilie tranquillité de la campagne, vitalité économique et touristique . En effet Plumelec regroupe tous les services nécessaire pour bien y vivre : nombreux artisans et commerces; nombreuses associations; nombreux équipements sportifs et culturels; un nouveau centre de secours comptant 33 pompiers volontaires ainsi qu'un pôle médical regroupant plusieurs professionnels de la santé : 1 médecin, un pédicure podologue, 4 infirmières, 3 kinés et un orthophoniste. Hors pôle médical la commune compte également un dentiste, un ostéopathe, un service associatif de soins et de maintien à domicile, un service", contact: .init(name: "Mairie de PLUMELEC", mail: ""), pict: [""], speciality: "Médecin généraliste", patientsList: true, accomodationProvided: true), Advertisement.init( title: "Médecin à Tours", town: "Tours", zipCode: "XXXXX", description: "En Morbihan Sud, la commune de Plumelec se situant à 20 min de Vannes,1 heure de Rennes et 1heure30 de Nantes recherche un médecin généraliste. Cette commune de 2750 habitants concilie tranquillité de la campagne, vitalité économique et touristique . En effet Plumelec regroupe tous les services nécessaire pour bien y vivre : nombreux artisans et commerces; nombreuses associations; nombreux équipements sportifs et culturels; un nouveau centre de secours comptant 33 pompiers volontaires ainsi qu'un pôle médical regroupant plusieurs professionnels de la santé : 1 médecin, un pédicure podologue, 4 infirmières, 3 kinés et un orthophoniste. Hors pôle médical la commune compte également un dentiste, un ostéopathe, un service associatif de soins et de maintien à domicile, un service", contact: .init(name: "Mairie de PLUMELEC", mail: ""), pict: [""], speciality: "Médecin généraliste", patientsList: true, accomodationProvided: true)]))
-//    
-//    let ads: [Advertisement] = [
-//        Advertisement( title: "Médecin généraliste à Plumebec (51420)", town: "Plumebec", zipCode: "51420", description: "En Morbihan Sud, la commune de Plumelec se situant à 20 min de Vannes,1 heure de Rennes et 1heure30 de Nantes recherche un médecin généraliste. Cette commune de 2750 habitants concilie tranquillité de la campagne, vitalité économique et touristique . En effet Plumelec regroupe tous les services nécessaire pour bien y vivre : nombreux artisans et commerces; nombreuses associations; nombreux équipements sportifs et culturels; un nouveau centre de secours comptant 33 pompiers volontaires ainsi qu'un pôle médical regroupant plusieurs professionnels de la santé : 1 médecin, un pédicure podologue, 4 infirmières, 3 kinés et un orthophoniste. Hors pôle médical la commune compte également un dentiste, un ostéopathe, un service associatif de soins et de maintien à domicile, un service", contact: Contact(name:  "Annie lhation", mail: "monmail@gg.com"), pict: ["Plumebec"], speciality: "", patientsList: true, accomodationProvided: true),
-//        Advertisement( title: "Recherche Médecin Généraliste", town: "La Capelle", zipCode: "02260", description: "Recherche 3ème Médecin Généraliste dans Maison de Santé ouverte depuis 01/2017.Primes à l\'installation par l'ARS et par la commune.Cadre de vie et d'exercice très agréable", contact: Contact(name:  "SOYEZ Fabrice", mail: "soyezfa@orange.fr", phone: "0781426761"), pict: ["Grande-synthe"], speciality: "", patientsList: false, accomodationProvided: true),
-//        Advertisement( title: "Médecin Généraliste Rechercher", town: "Grande-synthe", zipCode: "59760", description: "Suite au depart a la retraite de notre medecin generaliste, nous cherchons une personne qui pourrait reprendre le cabinet avec sa patientel. Tous les docteurs des alentours sont complets. Nous avons vraiment besoin d'un medecin generaliste", contact: Contact(name:  "Dufour", mail: "Maman-noumi@outlook.fr", phone: "0664558042"), pict: ["Grande-synthe"], speciality: "", patientsList: false, accomodationProvided: true)
-//        
-//    ]
+//    ListSearchRow(ad: Advertisement(title: "Médecin généraliste à Plumebec (51420)", town: "Plumebec", zipCode: "51420", description: "En Morbihan Sud, la commune de Plumelec se situant à 20 min de Vannes,1 heure de Rennes et 1heure30 de Nantes recherche un médecin généraliste. Cette commune de 2750 habitants concilie tranquillité de la campagne, vitalité économique et touristique . En effet Plumelec regroupe tous les services nécessaire pour bien y vivre : nombreux artisans et commerces; nombreuses associations; nombreux équipements sportifs et culturels; un nouveau centre de secours comptant 33 pompiers volontaires ainsi qu'un pôle médical regroupant plusieurs professionnels de la santé : 1 médecin, un pédicure podologue, 4 infirmières, 3 kinés et un orthophoniste. Hors pôle médical la commune compte également un dentiste, un ostéopathe, un service associatif de soins et de maintien à domicile, un service", contact: Contact(name:  "Annie lhation", mail: "monmail@gg.com"), pict: ["Plumebec"], speciality: "", patientsList: true, accomodationProvided: true), starIcon: ColoredIcons(nameIcon: <#T##String#>, nameIconColored: <#T##String#>, isColored: <#T##Bool#>), user: <#T##ProfilUser#>)
+//    ListSearchRow(ad: <#T##Advertisement#>, starIcon: <#T##ColoredIcons#>, user: <#T##ProfilUser#>)
+//    ListSearchRow(ad: <#T##Advertisement#>, starIcon: <#T##ColoredIcons#>, user: <#T##ProfilUser#>)
     
+    
+    /*
+     
+     */
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        List(ads){ ad in
+            NavigationLink {
+                DetailedAdUIView(ad: ad)
+            } label: {
+                AdDetailView(ad: ad)
+            }
+        }
     }
 }
 
